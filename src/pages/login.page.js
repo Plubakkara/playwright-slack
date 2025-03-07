e831ca24745831b47133fd5682714424c7ce2361
Akkaraphol Khanthamala <proplub@gmail.com>
2025-03-07 21:36:58 +0700

    first commit

---
+++ src/pages/login.page.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+
+// login.page.js
+
+export async function login(page) {
+    await page.goto('https://slack.com/signin#/signin', { timeout: 60000 });  // เพิ่มเวลา timeout
+
+    await page.waitForSelector('button#google_login_button', { timeout: 60000 });  // เพิ่มเวลา timeout
+
+    await page.click('button#google_login_button');
+
+    await page.waitForURL(/accounts.google.com/, { timeout: 60000 });  // เพิ่มเวลา timeout
+
+    await page.fill('input[type="email"]', process.env.EMAIL);
+
+    await page.keyboard.press('Enter');
+
+    await page.waitForSelector('input[type="password"]', { timeout: 60000 });  // เพิ่มเวลา timeout
+
+    await page.fill('input[type="password"]', process.env.PASSWORD);
+
+    await page.keyboard.press('Enter');
+
+    const proceedButton = await page.locator('button:has-text("ดำเนินการต่อ")');
+    await proceedButton.waitFor({ state: 'visible', timeout: 60000 });  // เพิ่มเวลา timeout
+    await proceedButton.click();
+    await page.waitForTimeout(3000);
+
+    // await page.waitForSelector('a[data-qa="ssb_redirect_open_in_browser"]:has-text("use Slack in your browser")', { timeout: 60000 });  // เพิ่มเวลา timeout
+    
+    // await page.click('a[data-qa="ssb_redirect_open_in_browser"]:has-text("use Slack in your browser")');
+
+    await page.goto('https://app.slack.com/client/T08GJMRP2HK', { timeout: 60000 });  // เพิ่มเวลา timeout
+
+    await page.waitForNavigation({ waitUntil: 'load', timeout: 60000 });  // เพิ่มเวลา timeout
+}
---
+++ src/tests/login.spec.js
@@ -0,0 +1,39 @@
+// login.spec.js
+import { test, expect } from '@playwright/test';
+import { login } from '../pages/login.page';
+
+test('Sign in with Google and proceed', async ({ page }) => {
+    await login(page);
+
+    console.log('Successfully signed in with Google');
+});
+
+test.only('Create group(My Quizzes)', async ({ page }) => {
+    await login(page);
+
+    await page.goto('https://app.slack.com/client/T08GJMRP2HK/D08HBBU21FS', { timeout: 60000 }); // รอ 1 นาที
+
+    console.log('Current URL:', page.url());
+        
+    const locatorCreateNewQuiz = '[aria-label="heavy plus sign emoji"]';
+    await page.click(locatorCreateNewQuiz);
+
+    const locatorDropdown = '[data-qa="QuizType"]';
+    await page.click(locatorDropdown);
+
+    await page.waitForSelector('text=Interactive Quiz');
+    await page.click('text=Interactive Quiz'); //Interactive Quiz
+
+    await page.waitForSelector('[aria-labelledby="Title-Title-label"]');
+    await page.fill('[aria-labelledby="Title-Title-label"]', 'ตัวอย่างที่ 1');
+
+    await page.waitForSelector('[aria-labelledby="Description-Description-label"]');
+    await page.fill('[aria-labelledby="Description-Description-label"]', 'รายละเอียด');
+
+    await page.click('[data-qa="wizard_modal_next"]');
+    
+    // await page.waitForTimeout(10000);
+    console.log('Successfully signed in with Google');
+});
+
+
